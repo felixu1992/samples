616bc28d1cd5af8d2e70cc31473c3b87d629c746
felixu <xufei_0320@163.com>
2020-07-02 11:09:35 +0800

    feat(jvm): 添加学习 jvm 工程

---
+++ jvm-demo/src/test/java/top/felixu/jvm/JvmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package top.felixu.jvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvmDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
